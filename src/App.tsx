f6b8ef6d71dd0c48892e78af127589a6914b0b8a
BSlaven <slaven.bunijevac@gmail.com>
2024-05-05 16:12:55 +0200

    introduce ItemCard and ItemPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import './App.css';
+
+import ItemCard from './components/ItemCard/ItemCard';
+// import ItemPage from './components/ItemPage/ItemPage';
 
 function App() {
   return (
     <>
-
+      <ItemCard />
     </>
   )
 }
