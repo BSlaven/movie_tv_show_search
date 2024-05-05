9787a7c5889e529c4ebe662ccd327df52c18e478
BSlaven <slaven.bunijevac@gmail.com>
2024-05-05 16:26:00 +0200

    change bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import ItemCard from './components/ItemCard/ItemCard';
 // import ItemPage from './components/ItemPage/ItemPage';
 
 function App() {
   return (
-    <>
-      <ItemCard />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/details" element={<h1>Details Page</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
