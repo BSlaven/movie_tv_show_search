02147b8817559720492f68bea76e22355e2e5e28
BSlaven <slaven.bunijevac@gmail.com>
2024-05-10 18:23:58 +0200

    add item card styling

--- src/components/ItemCard/ItemCard.tsx
+++ src/components/ItemCard/ItemCard.tsx
@@ -1,17 +1,17 @@
-import { ItemType } from "../Home/Home"
+import styles from './ItemCard.module.css';
 
 import { Link } from "react-router-dom"
 
 const ItemCard = ({ item }:any) => {
   return (
-    <Link to={`/details/${item.id}`}  className='card'>
-      <div className="card-image">
+    <Link to={`/details/${item.id}`}  className={styles.cardContainer}>
+      <div className={styles.cardImage}>
         <img 
           src={`https://image.tmdb.org/t/p/w500/${item.poster_path}.jpg`}
           alt="poster image"
         />
       </div>
-      <p className="item-title">
+      <p className={styles.cardTitle}>
         {item.title}
       </p>
     </Link>
